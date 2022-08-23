f412887bc6ae8ac3a6758b562a000b090ed605bd
Daniele Mansillo <danielemansillo@gmail.com>
2022-08-23 22:20:31 +0200

    delete staff

--- src/main/java/be/salushealthcare/salus/person/staff/Staff.java
+++ src/main/java/be/salushealthcare/salus/person/staff/Staff.java
@@ -1,15 +1,21 @@
 package be.salushealthcare.salus.person.staff;
 
 import be.salushealthcare.salus.person.Person;
+import be.salushealthcare.salus.timeslot.shiftslot.ShiftSlot;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 @Getter
 @SuperBuilder
 @NoArgsConstructor
 public class Staff extends Person {
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<ShiftSlot> allShiftSlots;
 }
